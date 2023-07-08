340bbcaf9c265e5bf63bf0839743bd6883eeec94
Ella Ordu <chinonyeordu15@gmail.com>
2023-07-08 12:07:52 +0100

    added assignment question

---
+++ src/game.dart
@@ -0,0 +1,11 @@
+// In your main function, generate a random number.
+
+// Initiate a infinite loop
+  // Receive input from the user on the terminal.
+  // Compare user input with random number.
+
+  // If input < random number, print "Too low" and continue
+  // If input > random number, print "Too high" and continue
+  // If input == random number, print "You guessed it!" and break
+
+// Print out "Good Bye from Developer Ella!"
